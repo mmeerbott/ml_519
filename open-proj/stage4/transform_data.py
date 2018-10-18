34b14d260561ad687d34b0e7403e6ce747a2b8d6
Michael M <ff.mmeerbott+nmsu@gmail.com>
2018-10-17 23:44:06 -0600

    Submission. Finished function to transform data that has the years in the header to be row based years.

--- open-proj/stage4/transform_data.py
+++ open-proj/stage4/transform_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 import argparse
+import csv
 from pandas import read_csv
 
 
@@ -11,11 +12,28 @@ def append_out(filename):
     return "{0}_{2}.{1}".format(*filename.rsplit('.', 1) + ['out'])
 
 
-def years_in_header():
+def years_in_header(filename):
     """ Transforms the years (+data) into individual rows
+        Requires the format to be:
+            state,[years,...]
     """
+    csvout = append_out(filename)
+    with open(filename, 'r') as infile, open(csvout, 'w') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
 
-    pass
+        oldheader = next(reader)  # first line is the header
+        writer.writerow(['state', 'year'])  # write new header
+
+        for oldrow in reader:
+            # get every year, and just turn it into a row with the same state
+            for i, year in enumerate(oldheader[1:]):
+                newrow = [oldrow[0]]  # state
+                newrow.extend([year, oldrow[i+1]])  # year and associated data
+                writer.writerow(newrow)
+                newrow = []  # reset the row we wrote
+
+    return csvout
 
 
 def average_no_years():
@@ -42,3 +60,6 @@ if __name__=="__main__":
 
     # TODO add options to reshape specific formats? 
     # might be better to just run by hand through console
+
+    # TODO remove this as it's a shortcut for testing
+    print(years_in_header('datasets/average_income.csv'))
